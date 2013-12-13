da8eeec0113311bd012145a1a589a1929592bea8
TheDrunkMafia <sean.hall412@gmail.com>
2013-12-13 23:58:11 +0000

    Forgot to commit
    Going off for the night

--- src/drunkmafia/mobilebase/tents/Tent.java
+++ src/drunkmafia/mobilebase/tents/Tent.java
@@ -8,33 +8,21 @@ import net.minecraftforge.common.ForgeDirection;
 public class Tent {
 	
 	private int[][][][] structure;
+	private int[] blockID;
 	protected int center;
 	
 	public void setStructure(int[][][][] structure){
 		this.structure = structure;
 	}
 	
+	public void setBlock(int[] block){
+		blockID = block;
+	}
+	
 	public boolean buildTent(World world, int x, int y, int z, ItemStack stack, ForgeDirection direction){
-		int tempX = x;
-		int tempZ = z;
-		switch(direction.ordinal() - 2){
-			case 0:
-				tempX = tempX - 4;
-				tempZ = tempZ - 4;
-				break;
-			case 1:
-				tempX = tempX - 4;
-				tempZ = tempZ - 4;
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				tempX = tempX - 4;
-				tempZ = tempZ - 4;
-				break;
-		}
-		System.out.println("r");
+		int tempX = x - 4;
+		int tempZ = z - 4;
+
 		for(int a1 = 0; a1 < structure[direction.ordinal() - 2].length; a1++){
 			for(int a2 = 0; a2 < structure[direction.ordinal() - 2][0].length; a2++){
 				for(int a3 = 0; a3 < structure[direction.ordinal() - 2][0][0].length; a3++){
@@ -43,6 +31,16 @@ public class Tent {
 						switch(temp){
 							case 1:
 								world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.cloth.blockID);
+								break;
+							case 2:
+								world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.doorWood.blockID);
+								break;
+							case 3:
+								world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.fence.blockID);
+								break;
+							case 4:
+								world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.torchWood.blockID, 5, 2);
+								break;
 						}
 					}
 				}
