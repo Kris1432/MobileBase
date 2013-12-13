5fd2f66cfc0ae057323bc42861064b2af191c492
TheDrunkMafia <sean.hall412@gmail.com>
2013-12-13 22:19:58 +0000

    Got it to work =D

--- src/drunkmafia/mobilebase/tents/Tent.java
+++ src/drunkmafia/mobilebase/tents/Tent.java
@@ -15,22 +15,24 @@ public class Tent {
 	}
 	
 	public boolean buildTent(World world, int x, int y, int z, ItemStack stack, ForgeDirection direction){
-		int i = 0;
 		int tempX = x;
-        int tempY = y - 1;
-        int tempZ = z;  
-        
+		int tempZ = z;
+		switch(direction.ordinal() - 2){
+			case 1:
+				tempX = tempX - 4;
+				tempZ = tempZ - 4;
+				System.out.println("r");
+				break;
+		}
+		System.out.println("r");
 		for(int a1 = 0; a1 < structure[direction.ordinal() - 2].length; a1++){
-			tempY++;
 			for(int a2 = 0; a2 < structure[direction.ordinal() - 2][0].length; a2++){
-				tempZ++;
 				for(int a3 = 0; a3 < structure[direction.ordinal() - 2][0][0].length; a3++){
-					tempX++;
 					int temp = structure[direction.ordinal() - 2][a1][a2][a3];
 					if(temp != 0){
 						switch(temp){
 							case 1:
-								world.setBlock(a3 + x, a1 + y, a2 + z, Block.cloth.blockID);
+								world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.cloth.blockID);
 						}
 					}
 				}
