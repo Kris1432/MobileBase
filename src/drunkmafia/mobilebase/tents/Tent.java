221d294d6bebc8637227ab62b0fedbdad95e43c0
TheDrunkMafia <sean.hall412@gmail.com>
2013-12-14 14:45:57 +0000

    Reworking the rebuilding of the inside

--- src/drunkmafia/mobilebase/tents/Tent.java
+++ src/drunkmafia/mobilebase/tents/Tent.java
@@ -62,7 +62,7 @@ public class Tent {
 					}
 				}
 			}
-			reBuildInside(world, x, y, z, tag);
+			//reBuildInside(world, x, y, z, tag);
 		}
 		return true;
 	}
@@ -96,17 +96,12 @@ public class Tent {
 		int tempX = x - 4;
 		int tempZ = z - 4;
 		int index = 0;
-		boolean run = false;
 		for(int a1 = 0; a1 < structure[direction.ordinal() - 2].length; a1++){
 			for(int a2 = 0; a2 < structure[direction.ordinal() - 2][0].length; a2++){
 				for(int a3 = 0; a3 < structure[direction.ordinal() - 2][0][0].length; a3++){
-					int temp = structure[direction.ordinal() - 2][a1][a2][a3];
-					if(!run && temp == 3)
-						run = true;
-					else if(temp == 3)
-						run = false;
-					
-					if(run && temp == 0){
+					int temp = structure[direction.ordinal() - 2][a1][a2][a3];					
+					if(temp == 5){
+						index++;
 						world.setBlock(a3 + tempX, a1 + y, a2 + tempZ, Block.glass.blockID);
 					}
 				}
