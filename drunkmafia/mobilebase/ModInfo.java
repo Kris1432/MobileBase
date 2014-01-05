7239a33a9199a3375cb5606067db268ac4806a4e
TheDrunkMafia <sean.hall412@gmail.com>
2014-01-05 18:00:08 +0000

    Added in update info

--- drunkmafia/mobilebase/ModInfo.java
+++ drunkmafia/mobilebase/ModInfo.java
@@ -4,10 +4,12 @@ public class ModInfo {
 	
 	public static final String MODID   = "mobilebase";
 	public static final String NAME    = "Mobile Base"; 
-	public static final String VERSION = "Pre-Alpha 0.01";
+	public static final String VERSION = "1.2";
 	
 	public static final String CHANNEL = MODID;
 	
 	public static final String PROXY_CLIENT = "drunkmafia.mobilebase.proxies.ClientProxy";
 	public static final String PROXY_SERVER = "drunkmafia.mobilebase.proxies.ServerProxy";
+	
+	public static String update_Info = "";
 }
