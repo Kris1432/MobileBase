df0b969496eb26ad0914048ee980afc851170118
TheDrunkMafia <sean.hall412@gmail.com>
2014-01-08 15:17:30 +0000

    Added in new entries for the blocks

--- drunkmafia/mobilebase/block/ModBlocks.java
+++ drunkmafia/mobilebase/block/ModBlocks.java
@@ -2,19 +2,29 @@ package drunkmafia.mobilebase.block;
 
 import net.minecraft.block.Block;
 import cpw.mods.fml.common.registry.GameRegistry;
+import drunkmafia.mobilebase.lib.BlockInfo;
+import drunkmafia.mobilebase.tileentity.*;
 
 public class ModBlocks {
 	
 	public static Block tentPost;
 	public static Block wool;
+	public static Block tentBuilder;
+	public static Block tentBluePrinter;
 	
 	public static void init(){
 		tentPost = new TentPostBlock();
 		wool = new WoolBlock();
+		tentBuilder = new TentBuilderBlock();
+		tentBluePrinter = new TentBluePrinterBlock();
 		
 		GameRegistry.registerBlock(wool, BlockInfo.wool_UnlocalizedName);
 		GameRegistry.registerBlock(tentPost, BlockInfo.post_UnlocalizedName);
+		GameRegistry.registerBlock(tentBuilder, BlockInfo.tentBuilder_UnlocalizedName);
+		
 		GameRegistry.registerTileEntity(TentPostTile.class, BlockInfo.post_tile);
 		GameRegistry.registerTileEntity(TentPostTileDummy.class, BlockInfo.postDummy_tile);
+		GameRegistry.registerTileEntity(TentBluePrinterTile.class, BlockInfo.tentBluePrinter_Tile);
+		GameRegistry.registerTileEntity(TentBuilderTile.class, BlockInfo.tentBuilder_Tile);
 	}
 }
--- drunkmafia/mobilebase/config/ConfigHandler.java
+++ drunkmafia/mobilebase/config/ConfigHandler.java
@@ -5,9 +5,9 @@ import java.util.logging.Level;
 
 import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.FMLLog;
-import drunkmafia.mobilebase.ModInfo;
-import drunkmafia.mobilebase.block.BlockInfo;
-import drunkmafia.mobilebase.item.ItemInfo;
+import drunkmafia.mobilebase.lib.BlockInfo;
+import drunkmafia.mobilebase.lib.ItemInfo;
+import drunkmafia.mobilebase.lib.ModInfo;
 
 public class ConfigHandler {
 	public static void init(File file){
@@ -19,6 +19,8 @@ public class ConfigHandler {
 			
 			BlockInfo.post_ID = config.getBlock(BlockInfo.post_UnlocalizedName, BlockInfo.post_Default_ID).getInt();
 			BlockInfo.wool_ID = config.getBlock(BlockInfo.wool_UnlocalizedName, BlockInfo.wool_Default_ID).getInt();
+			BlockInfo.tentBluePrinter_ID = config.getBlock(BlockInfo.tentBluePrinter_UnlocalizedName, BlockInfo.tentBluePrinter_Default_ID).getInt();
+			BlockInfo.tentBuilder_ID = config.getBlock(BlockInfo.tentBuilder_UnlocalizedName, BlockInfo.tentBuilder_Default_ID).getInt();
 			
 		}catch(Exception e){
 			FMLLog.log(Level.SEVERE, "[" + ModInfo.NAME + "] Config Error, please report this to the mod author", e);
