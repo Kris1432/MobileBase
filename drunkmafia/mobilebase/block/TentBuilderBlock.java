339ad4bbbad71516a3c3aea101f5e355d5f138c7
TheDrunkMafia <sean.hall412@gmail.com>
2014-01-08 15:16:10 +0000

    Created tiles for blocks

---
+++ drunkmafia/mobilebase/block/TentBuilderBlock.java
@@ -0,0 +1,34 @@
+package drunkmafia.mobilebase.block;
+
+import cpw.mods.fml.common.network.FMLNetworkHandler;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.common.network.ForgeNetworkHandler;
+import drunkmafia.mobilebase.MobileBase;
+import drunkmafia.mobilebase.lib.BlockInfo;
+import drunkmafia.mobilebase.tileentity.TentBuilderTile;
+
+public class TentBuilderBlock extends Block implements ITileEntityProvider{
+	public TentBuilderBlock() {
+		super(BlockInfo.tentBuilder_ID, Material.wood);
+		setUnlocalizedName(BlockInfo.tentBuilder_UnlocalizedName);
+		setCreativeTab(MobileBase.tab);
+		setTextureName("crafting_table");
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		if(world.isRemote) return false;
+		FMLNetworkHandler.openGui(player, MobileBase.instance, BlockInfo.tentBuilder_Gui_ID, world, x, y, z);
+		return true;
+	}
+	
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		return new TentBuilderTile();
+	}
+}
